1e83833900e146a949adf6575bdfeb0ccd17ab10
Mathias Gili Kouymtchian <mathias.gili@live.com>
2019-11-21 21:53:24 -0300

    refactor names

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Test
+
+Developer Technical Questions.
+
+It was decided to do the first and second exercises in the same project.
+1. The first problem is solved with the method ParseLOC
+    a.First parse out the all the LOC segments 
+        i. split the segment by '
+        i. parse out the all the LOC segments
+    b.Second populate an array with the 2nd and 3rd element of each segment with the method TackSecondThird
+        i.split the segment by "+"
+        ii.take the 2nd and 3rd element of each segment
+2. The first problem is solved with the method GetRefCodRefText
+    a. First read the file XML from XMLFile1
+    B. GetRefCodRefText recive the XML file
+    c. GetRefCodRefTex return a Dictionary with Keyvaluepair (Key - RefCode, Value - RefText) 
--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp4
             try
             {
 
-                SplitLOC(@"UNA:+.? '
+                ParseLOC(@"UNA:+.? '
                             UNB + UNOC:3 + 2021000969 + 4441963198 + 180525:1225 + 3VAL2MJV6EH9IX + KMSV7HMD + CUSDECU - IE++1++1'
                             UNH + EDIFACT + CUSDEC:D: 96B: UN:145050'
                             BGM + ZEM:::EX + 09SEE7JPUV5HC06IC6 + Z'
@@ -27,7 +27,7 @@ namespace ConsoleApp4
                             ");
                 XmlDocument doc = new XmlDocument();
                 doc.Load("../../../XMLFile1.xml");
-                RefCodRefText(doc);
+                GetRefCodRefText(doc);
             }
             catch (Exception)
             {
@@ -36,19 +36,7 @@ namespace ConsoleApp4
             }
         }
 
-        public static Dictionary<string, string> RefCodRefText(XmlDocument doc)
-        {
-            string MWB = doc.SelectSingleNode("//Reference[@RefCode='MWB']").InnerText;
-            string CAR = doc.SelectSingleNode("//Reference[@RefCode='CAR']").InnerText;
-            string TRV = doc.SelectSingleNode("//Reference[@RefCode='TRV']").InnerText;
-            Dictionary<string, string> values = new Dictionary<string, string>();
-            values.Add("MWB", MWB);
-            values.Add("CAR", CAR);
-            values.Add("TRV", TRV);
-            return values;
-
-        }
-        public static string[] SplitLOC(string stringWithLOC)
+        public static string[] ParseLOC(string stringWithLOC)
         {
             try
             {
@@ -59,16 +47,7 @@ namespace ConsoleApp4
                 {
                     if (!element.Contains("LOC"))
                     {
-
-                        string[] elementSplitByPlus = element.Split("+");
-                        if (elementSplitByPlus.Length > 1)
-                        {
-                            arrayWithoutLOC.Add(elementSplitByPlus[1]);
-                        }
-                        if (elementSplitByPlus.Length > 2)
-                        {
-                            arrayWithoutLOC.Add(elementSplitByPlus[2]);
-                        }
+                        TackSecondThird(arrayWithoutLOC, element);
                     }
                 }
 
@@ -79,6 +58,31 @@ namespace ConsoleApp4
 
                 throw ex;
             }
+        }
+
+        private static void TackSecondThird(List<string> arrayWithoutLOC, string element)
+        {
+            string[] elementSplitByPlus = element.Split("+");
+            if (elementSplitByPlus.Length > 1)
+            {
+                arrayWithoutLOC.Add(elementSplitByPlus[1]);
+            }
+            if (elementSplitByPlus.Length > 2)
+            {
+                arrayWithoutLOC.Add(elementSplitByPlus[2]);
+            }
+        }
+        public static Dictionary<string, string> GetRefCodRefText(XmlDocument doc)
+        {
+            string MWB = doc.SelectSingleNode("//Reference[@RefCode='MWB']").InnerText;
+            string CAR = doc.SelectSingleNode("//Reference[@RefCode='CAR']").InnerText;
+            string TRV = doc.SelectSingleNode("//Reference[@RefCode='TRV']").InnerText;
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            values.Add("MWB", MWB);
+            values.Add("CAR", CAR);
+            values.Add("TRV", TRV);
+            return values;
+
         }
     }
 }
